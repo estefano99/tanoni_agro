b344f008931e62a445396a5bea51bc4fba6705b5
estefa99 <estefanobugari@gmail.com>
2024-08-12 17:06:08 -0300

    Navbar, section y carrusel con scrollAnimation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,49 @@
-import { useState } from 'react';
+import { useState, useEffect } from "react";
+import logo from "../images/logo.png";
 
 const Navbar = () => {
-  const [language, setLanguage] = useState('ES');
+  const [language, setLanguage] = useState("ES");
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleLanguage = () => {
-    setLanguage((prevLanguage) => (prevLanguage === 'EN' ? 'ES' : 'EN'));
+    setLanguage((prevLanguage) => (prevLanguage === "EN" ? "ES" : "EN"));
   };
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
+  useEffect(() => {
+    const sr = ScrollReveal({
+      reset: true,
+      distance: "30px",
+      duration: 1000,
+      easing: "ease-in-out",
+    });
+
+    sr.reveal(".navbarReveal", { delay: 200 });
+    sr.reveal(".logoReveal", {
+      reset:false,
+      duration: 2000,
+      rotate: {
+        x: 1,
+        y: 100,
+      },
+    });
+  }, []);
+
   return (
-    <header className="absolute inset-x-0 top-0 z-50 bg-transparent">
-      <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+    <header className="fixed inset-x-0 top-0 z-50 bg-transparent flex justify-center navbarReveal">
+      <nav
+        className="flex items-center justify-between p-6 lg:px-8 w-[1120px]"
+        aria-label="Global"
+      >
         <div className="flex lg:flex-1">
           <a href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
             <img
-              className="h-12 w-auto"
-              src="logo.png"
+              className="h-12 w-auto logoReveal"
+              src={logo.src}
               alt="Logo"
               loading="lazy"
             />
@@ -45,49 +68,67 @@ const Navbar = () => {
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                className={!isOpen ? 'block' : 'hidden'}
+                className={!isOpen ? "block" : "hidden"}
               />
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 d="M6 18L18 6M6 6l12 12"
-                className={isOpen ? 'block' : 'hidden'}
+                className={isOpen ? "block" : "hidden"}
               />
             </svg>
           </button>
         </div>
         <div className="hidden lg:flex lg:gap-x-12">
-          <a href="#home" className="text-md font-semibold leading-6 text-gray-900">
+          <a
+            href="#home"
+            className="text-md font-semibold leading-6 text-gray-900"
+          >
             Inicio
           </a>
-          <a href="#services" className="text-md font-semibold leading-6 text-gray-900">
+          <a
+            href="#services"
+            className="text-md font-semibold leading-6 text-gray-900"
+          >
             Servicios
           </a>
-          <a href="\nosotros" className="text-md font-semibold leading-6 text-gray-900">
+          <a
+            href="\nosotros"
+            className="text-md font-semibold leading-6 text-gray-900"
+          >
             Nosotros
           </a>
-          <a href="#what-we-do" className="text-md font-semibold leading-6 text-gray-900">
+          <a
+            href="#what-we-do"
+            className="text-md font-semibold leading-6 text-gray-900"
+          >
             Qué hacemos
           </a>
-          <a href="#contact" className="text-md font-semibold leading-6 text-gray-900">
+          <a
+            href="#contact"
+            className="text-md font-semibold leading-6 text-gray-900"
+          >
             Contacto
           </a>
-          <button onClick={toggleLanguage} className="text-md font-semibold leading-6 text-gray-900">
+          <button
+            onClick={toggleLanguage}
+            className="text-md font-semibold leading-6 text-gray-900"
+          >
             {language}
           </button>
         </div>
       </nav>
-      <div className={`${isOpen ? 'block' : 'hidden'} lg:hidden`} role="dialog" aria-modal="true">
+      <div
+        className={`${isOpen ? "block" : "hidden"} lg:hidden`}
+        role="dialog"
+        aria-modal="true"
+      >
         <div className="fixed inset-0 z-50"></div>
         <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
-              <img
-                className="h-8 w-auto"
-                src="logo.jpg"
-                alt="Logo"
-              />
+              <img className="h-8 w-auto" src="logo.png" alt="Logo" />
             </a>
             <button
               type="button"
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,19 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
     extend: {
       fontSize: {
-        'clamp-xs': ['clamp(1rem, 2vw, 2rem)', '1.2'],
-        'clamp-sm': ['clamp(1.5rem, 3vw, 2.5rem)', '1.4'],
-        'clamp-md': ['clamp(2rem, 4vw, 3rem)', '1.6'],
-        'clamp-lg': ['clamp(2.5rem, 5vw, 4rem)', '1.8'],
-        'clamp-xl': ['clamp(3rem, 6vw, 5rem)', '2'],
+        "clamp-xs": ["clamp(1rem, 2vw, 2rem)", "1.2"],
+        "clamp-sm": ["clamp(1.5rem, 3vw, 2.5rem)", "1.4"],
+        "clamp-md": ["clamp(2rem, 4vw, 3rem)", "1.6"],
+        "clamp-lg": ["clamp(2.5rem, 5vw, 4rem)", "1.8"],
+        "clamp-xl": ["clamp(3rem, 6vw, 5rem)", "2"],
       },
-			backgroundImage: {
-        'tractor-image': "url('/tractor1.avif')",
-      }
+      backgroundImage: {
+        "tractor-image": "url('/src/images/tractor1.avif')",
+      },
     },
   },
-	plugins: [],
-}
+  plugins: [require('flowbite/plugin')],
+};
