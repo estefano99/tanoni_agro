aa446eff082f9f6010e0d88c5fb7c028d8179138
estefa99 <estefanobugari@gmail.com>
2024-09-11 00:04:28 -0300

    navbar nueva

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,7 @@ const svgArgentina = (
     xmlns="http://www.w3.org/2000/svg"
     id="flag-icons-ar"
     viewBox="0 0 640 480"
+    className="h-3.5 w-3.5 rounded-full me-2"
   >
     <path fill="#74acdf" d="M0 0h640v480H0z" />
     <path fill="#fff" d="M0 160h640v160H0z" />
@@ -42,177 +43,197 @@ const Navbar = () => {
 
     if (path !== "/") {
       navbar.classList.add("sticky", "text-black", "bg-slate-200");
-      navbar.classList.remove("fixed");
     } else {
-      navbar.classList.add("text-white");
+      navbar.classList.add("md:text-white","text-black");
     }
   }, []);
 
   return (
-    <header className="fixed inset-x-0 top-0 z-50 bg-transparent flex justify-center navbarReveal">
-      <nav
-        className="flex items-center justify-between py-3 lg:px-8 w-full max-w-screen-xl"
-        aria-label="Global"
-      >
-        <div className="flex lg:flex-1">
-          <a href="/" className="-m-1.5 p-1.5">
-            <span className="sr-only">Your Company</span>
-            <img
-              className="w-20 logoReveal"
-              src={logo.src}
-              alt="Logo"
-              loading="lazy"
-            />
-          </a>
-        </div>
-        <div className="flex lg:hidden">
-          <button
-            type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-            onClick={toggleMenu}
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                className={!isOpen ? "block" : "hidden"}
-              />
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 18L18 6M6 6l12 12"
-                className={isOpen ? "block" : "hidden"}
-              />
-            </svg>
-          </button>
-        </div>
-        <div className="hidden lg:flex lg:items-center lg:gap-x-12 link-styles">
+    <header className="fixed inset-x-0 top-0 z-50 bg-transparent navbarReveal">
+      <nav className="bg-white md:bg-transparent">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a
             href="/"
-            className="flex-1 text-md font-semibold leading-6 hover:scale-105 hover:outline outline-offset-8 outline-2 transition-all"
+            className="flex items-center space-x-3 rtl:space-x-reverse"
           >
-            Inicio
+            <img src={logo.src} className="h-8" alt="Tanoni Agro Logo" />
+            <span className="text-xl self-center md:text-2xl font-semibold whitespace-nowrap dark:text-white">
+              TanoniAgro
+            </span>
           </a>
-          <a
-            href="\nosotros"
-            className="flex-1 text-md font-semibold leading-6 hover:scale-105 hover:outline outline-offset-8 outline-2 transition-all"
-          >
-            Nosotros
-          </a>
-          <a
-            href="/quehacemos"
-            className="flex-1 text-md font-semibold leading-6 hover:scale-105 hover:outline outline-offset-8 outline-2 transition-all"
-          >
-            Qué hacemos
-          </a>
-          <a
-            href="\contacto"
-            className="flex-1 text-md font-semibold leading-6 hover:scale-105 hover:outline outline-offset-8 outline-2 transition-all"
-          >
-            Contacto
-          </a>
-          <button
-            onClick={toggleLanguage}
-            className={`${
-              language == "ES"
-                ? " "
-                : " "
-            } flex-1 flex justify-center gap-2 items-center text-md font-semibold leading-6 hover:scale-105 hover:outline outline-offset-8 outline-2 transition-all`}
-          >
-            {language === "ES" ? (
-              <>
-                <span className="ml-2">{language}</span>
-                {svgArgentina}
-              </>
-            ) : (
-              <>
-                <span className="ml-2">{language}</span>
-                {svgIngles}
-              </>
-            )}
-          </button>
-        </div>
-      </nav>
-      <div
-        className={`${isOpen ? "block" : "hidden"} lg:hidden`}
-        role="dialog"
-        aria-modal="true"
-      >
-        <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <img className="h-8 w-auto" src={logo.src} alt="Logo" />
-            </a>
+          <div className="flex items-center md:order-2 space-x-1 md:space-x-0 rtl:space-x-reverse">
             <button
               type="button"
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
-              onClick={toggleMenu}
+              data-dropdown-toggle="language-dropdown-menu"
+              className="inline-flex items-center font-medium justify-center px-4 py-2 text-sm rounded-lg cursor-pointer hover:bg-slate-500/60"
             >
-              <span className="sr-only">Close menu</span>
               <svg
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
+                className="w-5 h-5 rounded-full me-3"
                 aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                viewBox="0 0 3900 3900"
+              >
+                <path fill="#b22234" d="M0 0h7410v3900H0z" />
+                <path
+                  d="M0 450h7410m0 600H0m0 600h7410m0 600H0m0 600h7410m0 600H0"
+                  stroke="#fff"
+                  strokeWidth="300"
+                />
+                <path fill="#3c3b6e" d="M0 0h2964v2100H0z" />
+                <g fill="#fff">
+                  <g id="d">
+                    <g id="c">
+                      <g id="e">
+                        <g id="b">
+                          <path
+                            id="a"
+                            d="M247 90l70.534 217.082-184.66-134.164h228.253L176.466 307.082z"
+                          />
+                          <use xlinkHref="#a" y="420" />
+                          <use xlinkHref="#a" y="840" />
+                          <use xlinkHref="#a" y="1260" />
+                        </g>
+                        <use xlinkHref="#a" y="1680" />
+                      </g>
+                      <use xlinkHref="#b" x="247" y="210" />
+                    </g>
+                    <use xlinkHref="#c" x="494" />
+                  </g>
+                  <use xlinkHref="#d" x="988" />
+                  <use xlinkHref="#c" x="1976" />
+                  <use xlinkHref="#e" x="2470" />
+                </g>
+              </svg>
+              English (US)
+            </button>
+            <div
+              className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700"
+              id="language-dropdown-menu"
+            >
+              <ul className="py-2 font-medium" role="none">
+                <li>
+                  <button
+                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white"
+                    role="menuitem"
+                  >
+                    <div className="inline-flex items-center">
+                      <svg
+                        aria-hidden="true"
+                        className="h-3.5 w-3.5 rounded-full me-2"
+                        xmlns="http://www.w3.org/2000/svg"
+                        id="flag-icon-css-us"
+                        viewBox="0 0 512 512"
+                      >
+                        <g fillRule="evenodd">
+                          <g strokeWidth="1pt">
+                            <path
+                              fill="#bd3d44"
+                              d="M0 0h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0z"
+                              transform="scale(3.9385)"
+                            />
+                            <path
+                              fill="#fff"
+                              d="M0 10h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0z"
+                              transform="scale(3.9385)"
+                            />
+                          </g>
+                          <path
+                            fill="#192f5d"
+                            d="M0 0h98.8v70H0z"
+                            transform="scale(3.9385)"
+                          />
+                          <path
+                            fill="#fff"
+                            d="M8.2 3l1 2.8H12L9.7 7.5l.9 2.7-2.4-1.7L6 10.2l.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8H45l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7L74 8.5l-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9L92 7.5l1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm-74.1 7l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7H65zm16.4 0l1 2.8H86l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm-74 7l.8 2.8h3l-2.4 1.7.9 2.7-2.4-1.7L6 24.2l.9-2.7-2.4-1.7h3zm16.4 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8H45l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9L92 21.5l1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm-74.1 7l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7H65zm16.4 0l1 2.8H86l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm-74 7l.8 2.8h3l-2.4 1.7.9 2.7-2.4-1.7L6 38.2l.9-2.7-2.4-1.7h3zm16.4 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8H45l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9L92 35.5l1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm-74.1 7l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7H65zm16.4 0l1 2.8H86l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm-74 7l.8 2.8h3l-2.4 1.7.9 2.7-2.4-1.7L6 52.2l.9-2.7-2.4-1.7h3zm16.4 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8H45l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9L92 49.5l1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm-74.1 7l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7H65zm16.4 0l1 2.8H86l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm-74 7l.8 2.8h3l-2.4 1.7.9 2.7-2.4-1.7L6 66.2l.9-2.7-2.4-1.7h3zm16.4 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8H45l-2.4 1.7 1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9zm16.4 0l1 2.8h2.8l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h3zm16.5 0l.9 2.8h2.9l-2.3 1.7.9 2.7-2.4-1.7-2.3 1.7.9-2.7-2.4-1.7h2.9zm16.5 0l.9 2.8h2.9L92 63.5l1 2.7-2.4-1.7-2.4 1.7 1-2.7-2.4-1.7h2.9z"
+                            transform="scale(3.9385)"
+                          />
+                        </g>
+                      </svg>
+                      English (US)
+                    </div>
+                  </button>
+                </li>
+                <li>
+                  <button
+                    className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white"
+                    role="menuitem"
+                  >
+                    <div className="inline-flex items-center">
+                      {svgArgentina}
+                      Español (AR)
+                    </div>
+                  </button>
+                </li>
+              </ul>
+            </div>
+            <button
+              data-collapse-toggle="navbar-language"
+              type="button"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-language"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-5 h-5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 17 14"
               >
                 <path
+                  stroke="currentColor"
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
+                  strokeWidth="2"
+                  d="M1 1h15M1 7h15M1 13h15"
                 />
               </svg>
             </button>
           </div>
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
+          <div
+            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+            id="navbar-language"
+          >
+            <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
+              <li>
                 <a
-                  href="#home"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  href="/"
+                  className="block py-2 px-3 md:p-0 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-gray-400 transition-all"
+                  aria-current="page"
                 >
                   Inicio
                 </a>
+              </li>
+              <li>
                 <a
-                  href="#services"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Servicios
-                </a>
-                <a
-                  href="#about"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  href="/nosotros"
+                  className="block py-2 px-3 md:p-0 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-gray-400 transition-all"
                 >
                   Nosotros
                 </a>
+              </li>
+              <li>
                 <a
-                  href="#what-we-do"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  href="/quehacemos"
+                  className="block py-2 px-3 md:p-0 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-gray-400 transition-all"
                 >
-                  Qué hacemos
+                  Que hacemos
                 </a>
+              </li>
+              <li>
                 <a
-                  href="#contact"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  href="/contacto"
+                  className="block py-2 px-3 md:p-0 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-gray-400 transition-all"
                 >
                   Contacto
                 </a>
-              </div>
-            </div>
+              </li>
+            </ul>
           </div>
         </div>
-      </div>
+      </nav>
     </header>
   );
 };
